1edd1572dc6c4c50c62599e958b52a67458e2196
Florian Kissling <flo@6230.org>
2017-11-10 01:21:25 +0100

    Remove gatsby-remark-copy-linked-files

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,7 +28,6 @@ module.exports = {
             },
           },
           "gatsby-remark-prismjs",
-          "gatsby-remark-copy-linked-files",
           "gatsby-remark-smartypants",
         ],
       },
